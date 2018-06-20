5f794ebfbe2b8335adcceb868dc2f83252aebbf8
Teague Henry <trhenry@live.unc.edu>
2018-06-20 16:01:05 -0400

    Update net_apply.R

--- R/net_apply.R
+++ R/net_apply.R
@@ -421,7 +421,7 @@ toPlot = to_data_frame(netSampleStatSet)
 
   p = ggplot2::ggplot(toPlot[toPlot$p.thres,], ggplot2::aes(x = as.factor(net.names), y = diff, color = (adjusted.p < p.threshold)))+
     ggplot2::geom_boxplot() + ggplot2::coord_flip() + ggplot2::scale_color_manual(values = c("FALSE" ="black", "TRUE" ="red"),name = "Significant\nDifferences", labels = c(paste0("p > ", p.threshold),
-                                                                                                       paste0("p <", p.threshold))) +ggplot2::theme_classic()
+                                                                                                       paste0("p < ", p.threshold))) +ggplot2::theme_classic()
 
   if(missing(labels)){
     p = p+ ggplot2::labs(x = "Change Name", y = paste0("Difference from Original ", toPlot$stat.name[1]))
@@ -472,7 +472,7 @@ netGroup_ggPlot = function(netSampleStatSet, grouping.variable, labels, sort = "
 
   p = ggplot2::ggplot(toPlot[toPlot$p.thres,], ggplot2::aes(x = as.factor(net.names), y = nets.stat, color = (adjusted.p < p.threshold), fill = group))+
     ggplot2::geom_boxplot() + ggplot2::coord_flip() + ggplot2::scale_color_manual(values = c("FALSE" ="black", "TRUE" ="red"),name = "Significant\nDifferences", labels = c(paste0("p > ", p.threshold),
-                                                                                                       paste0("p <", p.threshold)))+
+                                                                                                       paste0("p < ", p.threshold)))+
     ggplot2::scale_fill_discrete(name = "Group")+ggplot2::theme_classic()
 
   if(missing(labels)){
@@ -522,7 +522,7 @@ netGroupDiff_ggPlot = function(netSampleStatSet, grouping.variable, labels, sort
   toPlot$diff = toPlot$nets.stat - toPlot$orig.stat
   p = ggplot2::ggplot(toPlot[toPlot$p.thres,], ggplot2::aes(x = as.factor(net.names), y = diff, color = (adjusted.p < p.threshold), fill = group))+
     ggplot2::geom_boxplot() + ggplot2::coord_flip() + ggplot2::scale_color_manual(values = c("FALSE" ="black", "TRUE" ="red"),name = "Significant\nDifferences", labels = c(paste0("p > ", p.threshold),
-                                                                                                                                                 paste0("p <", p.threshold)))+
+                                                                                                                                                 paste0("p < ", p.threshold)))+
     ggplot2::scale_fill_discrete(name = "Group")+ggplot2::theme_classic()
 
   if(missing(labels)){
